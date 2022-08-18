18c276879687366ca2ab1d076acfebbbcf699360
Abel Diaz <abeldiaz2307@gmail.com>
2022-08-17 20:43:17 -0600

    Change main

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+import logic.Classes;
+/**
+ *
+ * @author Abel Díaz
+ */
+public class Main {
+    public static void main(String[] args) {
+        Classes.ExecuteLogin();
+    }
+}
