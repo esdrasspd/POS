0780b10f4aca2e1d00bfedad3f475d5c0053efa6
Abel Diaz <abeldiaz2307@gmail.com>
2022-08-24 21:58:41 -0600

    agregue el formulario test

--- src/Forms/FormTest.java
+++ src/Forms/FormTest.java
@@ -30,7 +30,7 @@ public class FormTest extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("Test");
+        jLabel1.setText("Test java");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -39,7 +39,7 @@ public class FormTest extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(175, 175, 175)
                 .addComponent(jLabel1)
-                .addContainerGap(198, Short.MAX_VALUE))
+                .addContainerGap(167, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
